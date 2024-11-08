11a7ddfac6a3313c89c1a68416afa8db3ec72c89
Hemasri <111884197+Hemasri0502@users.noreply.github.com>
2024-11-08 22:09:21 +0530

    Calc.java

--- Calc.java
+++ Calc.java
@@ -1,19 +1,8 @@
-import java.util.Scanner;
-
 public class Calc {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to the Simple Calculator!");
-
-        System.out.print("Enter first number: ");
-        double num1 = scanner.nextDouble();
-
-        System.out.print("Enter second number: ");
-        double num2 = scanner.nextDouble();
-
-        System.out.print("Choose an operation (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
+        double num1 = 10.5;
+        double num2 = 5.5;
+        char operator = '+';
 
         double result = 0;
 
@@ -40,6 +29,6 @@ public class Calc {
                 return;
         }
 
-        System.out.println("The result is: " + result);
+        System.out.println("The result of " + num1 + " " + operator + " " + num2 + " is: " + result);
     }
 }
